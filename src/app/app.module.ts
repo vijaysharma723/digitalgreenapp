955ff70c1a5199534feb8c8a86fe0a061d1845e2
djux1990 <deepak.ux1990@gmail.com>
2020-02-06 18:48:25 +0530

    recording feature partial done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,34 @@
-import { NgModule, OnInit } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule, OnInit } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { IonicStorageModule } from "@ionic/storage";
+import { Media } from "@ionic-native/media/ngx";
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    IonicStorageModule.forRoot({
+      name: "digitalgreendb",
+      driverOrder: ["localstorage"]
+    })
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Media
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/session-recording-page/session-recording-page.page.ts
+++ src/app/session-recording-page/session-recording-page.page.ts
@@ -1,40 +1,97 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {SharedDataService} from '../shared-data.service';
-import {Router} from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { SharedDataService } from "../shared-data.service";
+import { Router } from "@angular/router";
+import { Media, MediaObject } from "@ionic-native/media/ngx";
+import { Storage } from "@ionic/storage";
+import {
+  MediaCapture,
+  MediaFile,
+  CaptureError,
+  CaptureAudioOptions
+} from "@ionic-native/media-capture/ngx";
+import { Platform } from "@ionic/angular";
 
+const MEDIA_STORAGE_KEY = "mediafiles";
 @Component({
-  selector: 'app-session-recording-page',
-  templateUrl: './session-recording-page.page.html',
-  styleUrls: ['./session-recording-page.page.scss'],
+  selector: "app-session-recording-page",
+  templateUrl: "./session-recording-page.page.html",
+  styleUrls: ["./session-recording-page.page.scss"]
 })
 export class SessionRecordingPagePage implements OnInit {
-
-sessionData: object;
-sessionid: string;
-topicName: string;
-recordStarted : boolean = false;
-  constructor(public router: Router, private route: ActivatedRoute, private sharedDataSevice: SharedDataService) { }
+  sessionData: object;
+  sessionid: string;
+  topicName: string;
+  recordStarted: boolean = false;
+  file: MediaObject;
+  filepath: string;
+  storageDirectory: any;
+  constructor(
+    public router: Router,
+    private route: ActivatedRoute,
+    private sharedDataSevice: SharedDataService,
+    private media: Media,
+    private platform: Platform,
+    private storage: Storage
+  ) {}
 
   ngOnInit() {
-    this.route.params.subscribe((params)=>{
-      console.log("params",params);      
-      this.sessionid = params['sessionid'];
+    this.route.params.subscribe(params => {
+      console.log("params", params);
+      this.sessionid = params["sessionid"];
       const filteredData = this.sharedDataSevice.getSessionById(this.sessionid);
-      this.sessionData = (filteredData.length>0) ? filteredData[0] : null;
-       this.topicName = params['topicname'];
-    })
+      this.sessionData = filteredData.length > 0 ? filteredData[0] : null;
+      this.topicName = params["topicname"];
+    });
   }
-
-  saveRecording() {
-    if(this.recordStarted) {
-    this.sharedDataSevice.updateSessionTopicData(this.sessionid, this.topicName, "filename");
-    this.router.navigate(['/sessiondetails', this.sessionid]);
+  stopRecording() {
+    this.file.stopRecord();
+    this.saveRecording(this.filepath);
   }
-
-  }
-
-  startRecording() {
+  startRecording(
+    data = {
+      username: "deepak",
+      sessionid: "1580986703204",
+      topicname: "improvement"
+    }
+  ) {
+    debugger;
     this.recordStarted = true;
+    let key = data.username + "_" + data.sessionid + "_" + data.topicname;
+    this.filepath =
+      data.username + "_" + data.sessionid + "_" + data.topicname + ".m4a";
+    console.log("driver: ", this.storage.driver);
+    // set a key/value
+
+    this.storage.set(MEDIA_STORAGE_KEY, JSON.stringify(this.filepath));
+
+    // Or to get a key/value pair
+    this.storage.get(MEDIA_STORAGE_KEY).then(val => {
+      debugger;
+      console.log("Your age is", val);
+    });
+    this.file = this.media.create(this.filepath);
+    this.file.startRecord();
+    this.file.onStatusUpdate.subscribe(status => console.log(status)); // fires when file status changes
+
+    this.file.onSuccess.subscribe(() => console.log("Action is successful"));
+
+    this.file.onError.subscribe(error => console.log("Error!", error));
+
+    // let options: CaptureAudioOptions = { limit: 3, duration: 180 };
+    // this.mediaCapture.captureAudio(options).then(
+    //   (data: MediaFile[]) => console.log(data),
+    //   (err: CaptureError) => console.error(err)
+    // );
+  }
+  saveRecording(filename) {
+    if (this.recordStarted) {
+      this.sharedDataSevice.updateSessionTopicData(
+        this.sessionid,
+        this.topicName,
+        filename
+      );
+      this.router.navigate(["/sessiondetails", this.sessionid]);
+    }
   }
 }
