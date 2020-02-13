ee294249883d7ead252edfd8003294ee872f0e61
rishabh kalra <rishabh.kalra@niit.com>
2020-02-13 18:48:27 +0530

    changed footer css\

--- src/app/services/sync/sync.service.ts
+++ src/app/services/sync/sync.service.ts
@@ -16,9 +16,9 @@ import { SessionService } from '../session/session.service';
 })
 export class SyncService {
 
-  private APIEndpoint = 'http://13.234.2.81:3001/sessions/upload';
-  private createSessionEndPoint = 'http://13.234.2.81:3001/sessions/create';
-  private CheckStatusAPIEndpoint = 'http://13.234.2.81:3001/sessions/status/';
+  private APIEndpoint = 'https://ecdf37aa.ngrok.io/sessions/upload';
+  private createSessionEndPoint = 'https://ecdf37aa.ngrok.io/sessions/create';
+  private CheckStatusAPIEndpoint = 'https://ecdf37aa.ngrok.io/sessions/status/';
   private defaultBearer = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJpc2hhYmhrYWxyYTk2IiwiZW1haWwiOiJyaXNoYWJoa2FscmE5NkBnbWFpbC5jb20iLCJpYXQiOjE1ODA4ODI1Nzl9.dxrWrjX3jaUe4t33Y9H0oLdSxenSaJA-EYaCNHIk8Ys';
   parentFolderDir = 'session';
 
