ff61390741d59466e57b45e5adf5c1bd5bbbe1d6
rishabh kalra <rishabh.kalra@niit.com>
2020-03-12 11:34:54 +0530

    date is now getting translated properly

--- src/app/shared/pipes/datetime/date-time.pipe.ts
+++ src/app/shared/pipes/datetime/date-time.pipe.ts
@@ -2,13 +2,61 @@ import { Pipe, PipeTransform } from "@angular/core";
 import { TranslateService } from '@ngx-translate/core';
 
 @Pipe({
-  name: "dateTime"
+  name: 'dateTime',
+  pure: false,
 })
 export class DateTimePipe implements PipeTransform {
   month = {};
   timeZone = [];
 
-  constructor(private readonly translate: TranslateService) {
+  constructor(private readonly translate: TranslateService) {}
+
+  transform(value: string, args?: any[]): string {
+    // "2020-02-12T06:04:24.807Z"
+    this.updateDateFormatLang();
+    const dateObj = new Date(value);
+    // converting to dd/mm/yyyy format
+    let convertedDate = dateObj.getDate().toString();
+    const month = dateObj.getMonth() + 1;
+    convertedDate += " " + this.month[month];
+    convertedDate += "," + dateObj.getFullYear();
+
+    let timeZone = null;
+    let hour = dateObj.getHours();
+    let zoneEn = null;
+    if (this.translate.currentLang === 'en') {
+      timeZone = 'at';
+      zoneEn = hour >= 12 ? 'PM' : 'AM';
+
+    } else {
+      if (hour >= 5 && hour < 12) {
+        timeZone = this.timeZone[0];
+      } else if (hour >= 12 && hour < 16) {
+        timeZone = this.timeZone[1];
+      } else if (hour >= 16 && hour < 20) {
+        timeZone = this.timeZone[2];
+      } else {
+        timeZone = this.timeZone[3];
+      }
+    }
+
+    let convertedTime = timeZone + " ";
+
+    if ( hour > 12) {
+      hour -= 12;
+    }
+    convertedTime += hour.toString();
+    if (this.translate.currentLang === 'en') {
+      convertedTime += ' ' + zoneEn;
+    } else {
+      convertedTime += ' ' + 'बजे';
+    }
+
+    const convertedDateTime = convertedDate + "  " + convertedTime;
+    return convertedDateTime;
+  }
+
+  updateDateFormatLang() {
     this.month = {
       1: this.translate.instant('January'),
       2: this.translate.instant('February'),
@@ -29,40 +77,5 @@ export class DateTimePipe implements PipeTransform {
       this.translate.instant('Evening'),
       this.translate.instant('Night'),
     ];
-  }
-
-  transform(value: string, args?: any[]): string {
-    // "2020-02-12T06:04:24.807Z"
-    debugger;
-    const dateObj = new Date(value);
-    // converting to dd/mm/yyyy format
-    let convertedDate = dateObj.getDate().toString();
-    const month = dateObj.getMonth() + 1;
-    convertedDate += " " + this.month[month];
-    convertedDate += "," + dateObj.getFullYear();
-
-    let timeZone = null;
-    let hour = dateObj.getHours();
-
-    if (hour >= 5 && hour < 12) {
-      timeZone = this.timeZone[0];
-    } else if(hour >= 12 && hour < 16) {
-      timeZone = this.timeZone[1];
-    } else if(hour >= 16 && hour < 20) {
-      timeZone = this.timeZone[2];
-    } else {
-      timeZone = this.timeZone[3];
-    }
-
-    let convertedTime = timeZone + " ";
-
-    if(hour > 12) {
-      hour -= 12;
-    }
-    convertedTime += hour.toString();
-    // convertedTime += " " + "बजे";
-
-    const convertedDateTime = convertedDate + "  " + convertedTime;
-    return convertedDateTime;
   }
 }
