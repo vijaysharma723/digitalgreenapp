4249fca75326710e1f8d646b286c1d27c9b876a8
rishabh kalra <rishabh.kalra@niit.com>
2020-04-19 12:33:56 +0530

    lint fix in best practices module

--- src/app/bestpractices/bestpractices.component.ts
+++ src/app/bestpractices/bestpractices.component.ts
@@ -1,8 +1,6 @@
-import { Component, Injectable,  ChangeDetectorRef, Input } from '@angular/core';
-import { TranslateService} from '@ngx-translate/core';
+import { Component, Injectable, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { Platform } from '@ionic/angular';
-import { File} from "@ionic-native/file/ngx";
-import { Media, MediaObject } from "@ionic-native/media/ngx";
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -12,71 +10,56 @@ import { Subscription } from 'rxjs';
 })
 
 @Injectable({
-providedIn:'root'
+  providedIn: 'root'
 })
 
-export class BestpracticesComponent {
+export class BestpracticesComponent implements OnDestroy {
   filepath: any;
-   audio: any;
+  audio: any;
   stop: any;
-  audio1Path = './../../assets/sampleaudios'
+  audio1Path = './../../assets/sampleaudios';
   translateChangeSub: Subscription;
   constructor(
     private readonly translate: TranslateService,
     private cdr: ChangeDetectorRef,
     private plt: Platform,
-    private file: File,
-    private media: Media,
-    ){}
-    
+  ) { }
 
-    mediaPauseAudio(idx) {
-      this.audio.pause();
-      this.stop = undefined;
-      this.cdr.detectChanges();
-    }
-    
-    mediaPlayAudio(idx ) {
-      if(this.plt.is("android"))
-  { 
 
-      if(this.stop == 1 || this.stop ==2)
-      {
+  mediaPauseAudio(idx) {
+    this.audio.pause();
+    this.stop = undefined;
+    this.cdr.detectChanges();
+  }
+
+  mediaPlayAudio(idx) {
+    if (this.plt.is('android')) {
+      if (this.stop === 1 || this.stop === 2) {
         this.audio.pause();
       }
-      if (this.plt.is("android")) {
+      if (this.plt.is('android')) {
         this.filepath = this.getSampleAudioPath(idx);
         this.audio = new Audio(this.filepath);
-        }
-      this.stop = idx;
-     
-      this.audio.play();
-     this.audio.setVolume = 1.0; 
-      this.audio.addEventListener("ended",()=>{
-        this.mediaPauseAudio(idx);
-      })
-    
-    
-         
-  }    
-     }
-
-    getSampleAudioPath(audioID) {
-      //return the file path corresponding to the audio sample  ID provideded 
-      return this.audio1Path + `/sample${audioID}.wav`;
-     }
-  
-      ngOnDestroy() {
-      if(this.audio) {
-        this.audio.pause();
-        this.stop =undefined;
-        this.audio = null;
       }
+      this.stop = idx;
+      this.audio.play();
+      this.audio.setVolume = 1.0;
+      this.audio.addEventListener('ended', () => {
+        this.mediaPauseAudio(idx);
+      });
     }
+  }
 
+  getSampleAudioPath(audioID) {
+    // return the file path corresponding to the audio sample  ID provideded
+    return this.audio1Path + `/sample${audioID}.wav`;
+  }
 
-    hello()
-    {
-      console.log("hello world");
+  ngOnDestroy() {
+    if (this.audio) {
+      this.audio.pause();
+      this.stop = undefined;
+      this.audio = null;
     }
+  }
 }
